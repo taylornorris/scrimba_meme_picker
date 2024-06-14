c06a8384a6f80b23c374138d0970b7420fa94449
taylornorris <taylornorrisdev@gmail.com>
2024-06-14 10:10:32 -0600

    End of day 1

--- index.js
+++ index.js
@@ -169,12 +169,27 @@ const catsData = [
     },
 ]
 
-function getEmotionsArray(cats){
+const emotionRadios = document.getElementById("emotion-radios")
+
+function getEmotionsArray(cats) {
     const emotionsArray = []
-    for (let i = 0; i < cats.length; i++){
-        for (let j=0; j < cats[i].emotionTags.length; j++){
-                emotionsArray.push(cats[i].emotionTags[j])
+
+    for (let cat of cats) {
+        for (let emotion of cat.emotionTags) {
+            emotionsArray.push(emotion)
         }
     }
-console.log(emotionsArray)
+    return emotionsArray
 }
+
+function renderEmotionsRadios(cats) {
+    const emotions = getEmotionsArray(cats)
+    let radioItems = ""
+
+    for (let emotion of emotions) {
+        radioItems += `<p>${emotion}</p>`
+    }
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(catsData)
